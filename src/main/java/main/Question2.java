3d51f92d9a2df276840eb46a197924b0d124e812
kannan177 <kannanpoonkodi177@.github.com>
2019-08-16 10:33:09 +0530

    GOOD PROGRAMING

--- src/main/java/main/Question2.java
+++ src/main/java/main/Question2.java
@@ -17,11 +17,12 @@ public static void main(String[] args) {
 		numbers.add(Integer.parseInt(sc.nextLine()));
 	Question2 qns2=new Question2();
 	System.out.println(qns2.listWithDuplicates(numbers));
-
+	sc.close();
 }
 public List<Integer> listWithDuplicates(List<Integer> number){
-	List <Integer> L1 = null;
+	List <Integer> L1 = new ArrayList<Integer>();
 	//write your code here !! change the L1 to required value
+	L1.addAll(number);
 	return L1 ;
 }
 }
